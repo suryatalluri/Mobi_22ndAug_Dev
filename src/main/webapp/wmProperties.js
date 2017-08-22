401695c104f738b2380dafe06061412cc702c088
surya talluri <surya.talluri@wm9.com>
2017-08-22 05:03:09 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Mobi 22ndAug Dev",
+  "homePage" : "Main",
+  "name" : "Mobi_22ndAug_Dev",
+  "platformType" : "MOBILE",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
